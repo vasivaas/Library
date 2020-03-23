6ac2ca5e56746f36484fc9676c1039831e7e4e36
Vasyl Kitsyla <wkichira@gmail.com>
2020-03-23 23:09:11 +0200

    Update views.py

--- library/library/catalog/views.py
+++ library/library/catalog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -20,7 +21,9 @@ def index(request):
         name__iexact='Poem').count()
 
     list_genre = Genre.objects.all()
-    #num_genre = Genre.objects.filter(name__icontains=list_genre.name).count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    # num_genre = Genre.objects.filter(name__icontains=list_genre.name).count()
     # генерація HTML шаблона index.html з даними всередині
     # змінною контексту context
     return render(
@@ -31,7 +34,8 @@ def index(request):
             'num_authors': authors,
             'num_book_and_genre': num_book_and_genre,
             'list_genre': list_genre,
-            #'num_genre': num_genre,
+            'num_visits': num_visits,
+            # 'num_genre': num_genre,
         }
     )
 
@@ -76,5 +80,26 @@ class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     template_name = 'catalog/bookinstance_list_borrowed_all.html'
     paginate_by = 10
 
+    @property
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+from .forms import CreateUserForm
+from django.contrib import messages
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Акаунт створив ' + user)
+                return redirect('login')
+        context = {'form': form, }
+        return render(request, 'register/register.html', context)
