cad09e0da39787f428d8a8dd4a16bca83c29af6b
Vasyl Kitsyla <wkichira@gmail.com>
2020-04-04 20:26:22 +0300

    Update forms.py

--- library/library/catalog/forms.py
+++ library/library/catalog/forms.py
@@ -2,6 +2,52 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime  # for checking renewal date range.
+from .models import BookInstance
+
+'''
+
+class RenewBookForm(forms.Form):
+    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
+    def clean_renewal_date(self):
+        data = self.cleaned_data['renewal_date']
+
+        
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past'))
+
+        
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+        
+        return data
+'''
+
+
+class RenewBookModelForm(ModelForm):
+    def clean_due_back(self):
+        data = self.cleaned_data['due_back']
+
+        # Перевірка того, що дата не в минулому часі
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past'))
+
+        # Перевірка що дата не перевищує ліміт в 4 тижні (+4 weeks)
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+        # Повернення очищених даних
+        return data
+
+    class Meta:
+        model = BookInstance
+        fields = ['due_back', ]
+        labels = {'due_back': _('Renewal date'), }
+        help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).'), }
 
 
 class CreateUserForm(UserCreationForm):
